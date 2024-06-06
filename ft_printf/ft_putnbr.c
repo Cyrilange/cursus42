ecd08853848c85f45d766a5d3f3ddd0d7f6e414d
Cyril Salamite <106234259+Cyrilange@users.noreply.github.com>
2024-06-06 11:10:40 +0200

    Update ft_putnbr.c

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -1,30 +1,15 @@
-
+ 
 
 #include "ft_printf.h"
 
-int ft_putnbr(int num)
-{
-    int len = 0;
-
-    if (num == -2147483648)
-    {
-        len += ft_putchar('-');
-        len += ft_putchar('2');
-        num = 147483648;
+int ft_putnbr(int n) {
+    if (n < 0) {
+        ft_putchar('-');
+        n = -n;
     }
-    else if (num < 0)
-    {
-        len += ft_putchar('-');
-        num = - num;
+    if (n >= 10) {
+        ft_putnbr(n / 10);
     }
-    if (num >= 10)
-    {
-        len += ft_putnbr(num / 10);
-        len += ft_putnbr(num % 10);
-    }
-    else
-    {
-        len += ft_putchar(num + '0');
-    }
-    return len;
+    ft_putchar(n % 10 + '0');
+    return (1);
 }
